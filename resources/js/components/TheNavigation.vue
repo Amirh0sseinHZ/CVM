19ad2eab33b8af9dcf16fa6c27c1a3b62c7527b6
k1lleR8 <amirziaee12@gmail.com>
2021-02-04 12:53:39 +0200

    Add class to the router links

--- resources/js/components/TheNavigation.vue
+++ resources/js/components/TheNavigation.vue
@@ -1,6 +1,6 @@
 <template>
-    <header class="fixed">
-        <router-link :to="{ name: 'Home' }">Home</router-link>
-        <router-link :to="{ name: 'Login' }">Login</router-link>
-    </header>
+    <div class="text-xl select-none outline-none">
+        <router-link class="router-link" :to="{ name: 'Home' }">Home</router-link>
+        <router-link class="router-link" :to="{ name: 'Login' }">Login</router-link>
+    </div>
 </template>
