d9b8b76b82d88875de0586c7aa33723e666f25e9
k1lleR8 <amirziaee12@gmail.com>
2021-02-04 12:54:06 +0200

    New topbar component

---
+++ resources/js/components/TheTopbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="fixed top-0 w-full h-24 flex flex-row justify-between items-center px-12">
+        <div>
+            <svg width="30" height="30" class="rounded-xl"
+                 xmlns="http://www.w3.org/2000/svg">
+                <image :href="'./img/nfq-logo.svg'" width="30" height="30"/>
+            </svg>
+        </div>
+        <the-navigation></the-navigation>
+    </header>
+</template>
+
+<script>
+import TheNavigation from "./TheNavigation";
+
+export default {
+    components: {
+        TheNavigation
+    }
+}
+</script>
