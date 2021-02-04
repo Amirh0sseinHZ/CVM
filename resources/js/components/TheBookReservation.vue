e127c485b7308cad0c27eb5786a95d08c5e3ee50
k1lleR8 <amirziaee12@gmail.com>
2021-02-04 12:54:19 +0200

    New book reservation component

---
+++ resources/js/components/TheBookReservation.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <button class="select-none outline-none draw-border cursor-pointer py-6 px-12 text-xl tracking-wide">Book an Appointment</button>
+    </div>
+</template>
+
+<style scoped>
+.draw-border {
+    box-shadow: inset 0 0 0 4px #1C91DA;
+    color: #1C91DA;
+    transition: color 0.25s 0.0833333333s;
+    position: relative;
+}
+.draw-border::before, .draw-border::after {
+    border: 0 solid transparent;
+    box-sizing: border-box;
+    content: "";
+    pointer-events: none;
+    position: absolute;
+    width: 0;
+    height: 0;
+    bottom: 0;
+    right: 0;
+}
+.draw-border::before {
+    border-bottom-width: 4px;
+    border-left-width: 4px;
+}
+.draw-border::after {
+    border-top-width: 4px;
+    border-right-width: 4px;
+}
+.draw-border:hover {
+    color: #ffe593;
+}
+.draw-border:hover::before, .draw-border:hover::after {
+    border-color: #ffe593;
+    transition: border-color 0s, width 0.25s, height 0.25s;
+    width: 100%;
+    height: 100%;
+}
+.draw-border:hover::before {
+    transition-delay: 0s, 0s, 0.25s;
+}
+.draw-border:hover::after {
+    transition-delay: 0s, 0.25s, 0s;
+}
+</style>
