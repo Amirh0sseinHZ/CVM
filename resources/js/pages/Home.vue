33513cd78d99699adb6516c7c1c178184f2be42a
k1lleR8 <amirziaee12@gmail.com>
2021-02-04 12:56:13 +0200

    Add children components & logic of render

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
-        <h1>Home Page</h1>
+        <the-reservation-details v-if="hasActiveReservation"></the-reservation-details>
+        <the-book-reservation v-else></the-book-reservation>
     </div>
 </template>
+
+<script>
+import TheBookReservation from '../components/TheBookReservation';
+import TheReservationDetails from '../components/TheReservationDetails';
+
+export default {
+    data() {
+        return {
+            hasActiveReservation: false
+        };
+    },
+    components: {
+        TheBookReservation,
+        TheReservationDetails
+    }
+}
+</script>
