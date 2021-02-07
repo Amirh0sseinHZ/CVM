0ed563638dbc19771c289e7094f978b7c5c4c473
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 01:21:37 +0200

    Vuex module to handle auth states

---
+++ resources/js/store/modules/auth.js
@@ -0,0 +1,47 @@
+import axios from "@/services/axios";
+import {getError} from "../../utils/helpers";
+
+export default {
+    state: {
+        status: localStorage.getItem('authStatus') || '',
+        user: ''
+    },
+    mutations: {
+        login_success(state, user) {
+            localStorage.setItem('authStatus', 1)
+            state.status = true
+            state.user = user
+        },
+        logout_success(state) {
+            localStorage.removeItem('authStatus')
+            state.status = ''
+            state.user = ''
+        }
+    },
+    actions: {
+        login({commit}, payload) {
+            return new Promise((resolve, reject) => {
+                axios
+                    .get("/sanctum/csrf-cookie")
+                    .then(() => {return axios.post("/login", payload)})
+                    .then(() => {return axios.get("/api/v1/user")})
+                    .then(response => {
+                        commit('login_success', response.data)
+                        resolve()
+                    }).catch(e => {reject(getError(e))});
+            });
+        },
+        async logout({commit}) {
+            return new Promise((resolve, reject) => {
+                axios.post("/logout").then(() => {
+                    commit("logout_success");
+                    resolve();
+                }).catch(() => {reject()});
+            });
+        }
+    },
+    getters: {
+        isLoggedIn: state => !!state.status,
+        stateUser: state => state.user
+    }
+}
