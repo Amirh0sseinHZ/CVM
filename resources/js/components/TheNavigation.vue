d8f835451e03dfd384710d7d085efaa9d4248bc1
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 01:09:05 +0200

    Change to conditional rendering based on authentication status & add logout functionality

--- resources/js/components/TheNavigation.vue
+++ resources/js/components/TheNavigation.vue
@@ -1,6 +1,25 @@
 <template>
-    <div class="text-xl select-none outline-none">
+    <div v-if="!isLoggedIn" class="text-xl select-none outline-none">
         <router-link class="router-link" :to="{ name: 'Home' }">Home</router-link>
         <router-link class="router-link" :to="{ name: 'Login' }">Login</router-link>
     </div>
+    <div v-else class="text-xl select-none outline-none">
+        <router-link class="router-link" :to="{ name: 'Dashboard' }">Dashboard</router-link>
+        <a @click="logout" class="router-link router-link-active">Logout</a>
+    </div>
 </template>
+
+<script>
+export default {
+    computed: {
+        isLoggedIn: function() {return this.$store.getters.isLoggedIn}
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch('logout').then(() => {
+                this.$router.push({ name: 'Login'});
+            });
+        }
+    }
+}
+</script>
