4ae68d1367ae1e5755b95bb5659f9bfffef92831
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 01:09:13 +0200

    Add black color

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         transparent: 'transparent',
         current: 'currentColor',
         white: '#ffffff',
+        black: '#000000',
         gray: '#8798A7',
         yellow: '#ffe593',
         blue: {
