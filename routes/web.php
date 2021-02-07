8ca8a95b435f1c4360ce894e81a704643b7b6611
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 01:04:10 +0200

    Move out API specific routes to its own file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,19 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::prefix('api/v1')->group(function () {
-    Route::prefix('reservations')->group(function () {
-        /* Todo: protect the route */
-        Route::get('/', [ReservationController::class, 'index']);
-        /***************************/
-        Route::post('/', [ReservationController::class, 'store']);
-        Route::get('/{code}', [ReservationController::class, 'show'])
-            ->where('code', '^([1-9]\d{0,}_[1-9]\d{0,})$');
-        Route::put('/{code}/cancel', [ReservationController::class, 'cancel'])
-            ->where('code', '^([1-9]\d{0,}_[1-9]\d{0,})$');
-    });
-});
 
 Route::get('/', function () {
     return view('index');
