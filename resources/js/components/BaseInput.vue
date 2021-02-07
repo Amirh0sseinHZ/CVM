cfb3bec1734a235e3a279a46d71cdad67432733c
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 01:22:00 +0200

    Add base input component

---
+++ resources/js/components/BaseInput.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <label
+      :for="name"
+      class="text-sm text-gray-500"
+      :class="{ 'sr-only': !showLabel }"
+      v-if="label"
+    >
+      {{ label }}
+    </label>
+    <input
+      :id="name"
+      :type="type"
+      :value="value"
+      :required="required"
+      @input="updateValue"
+      :placeholder="placeholder"
+      :autocomplete="autocomplete"
+      class="block text-black w-full px-3 py-2 placeholder-gray border border-gray rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue focus:border-blue"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  inheritAttrs: false,
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: [String, Number],
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+    showLabel: {
+      type: Boolean,
+      default: true,
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    placeholder: {
+      type: String,
+      default: null,
+    },
+    autocomplete: {
+      type: String,
+      default: null,
+    },
+  },
+  methods: {
+    updateValue(event) {
+      return this.$emit("input", event.target.value);
+    },
+  },
+};
+</script>
