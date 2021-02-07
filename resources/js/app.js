b90a56df3ca51c01c32cc3953645a9a0634a6250
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 00:19:57 +0200

    Now support notifications and Vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,18 @@ require('./bootstrap');
 
 import Vue from 'vue';
 import router from './router';
+import store from "./store";
+import Notifications from 'vue-notification'
 
 Vue.component(
     'App',
     require('./components/App.vue').default
 );
 
+Vue.use(Notifications);
+
 const app = new Vue({
-    el: '#app',
-    router
+    router,
+    store,
+    el: '#app'
 });
