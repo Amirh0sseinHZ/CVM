e1be3daca719ecfc2920322cf791b3431574065d
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 01:04:49 +0200

    Enable back CSRF

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,8 +11,5 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array
      */
-    protected $except = [
-        // Todo: enable CSRF
-        '*'
-    ];
+    protected $except = [];
 }
