7a04e61ac31866a791e33d8168172a81d60479fd
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 01:07:42 +0200

    Wrap the logo with the link to Home page

--- resources/js/components/TheTopbar.vue
+++ resources/js/components/TheTopbar.vue
@@ -1,19 +1,20 @@
 <template>
     <header class="fixed top-0 w-full h-24 flex flex-row justify-between items-center px-12">
         <div>
-            <svg width="30" height="30" class="rounded-xl"
-                 xmlns="http://www.w3.org/2000/svg">
-                <image :href="'./img/nfq-logo.svg'" width="30" height="30"/>
-            </svg>
+            <router-link :to="{ name: 'Home'}">
+                <svg width="30" height="30" class="rounded-xl" xmlns="http://www.w3.org/2000/svg">
+                    <image :href="'./img/nfq-logo.svg'" width="30" height="30"/>
+                </svg>
+            </router-link>
         </div>
         <the-navigation></the-navigation>
     </header>
 </template>
 
 <script>
-import TheNavigation from "./TheNavigation";
+    import TheNavigation from "./TheNavigation";
 
-export default {
+    export default {
     components: {
         TheNavigation
     }
