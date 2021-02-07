aeb84597fe3e0c4a22e519c995129ca3b3a4c9cf
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 00:12:08 +0200

    Add new routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SpecialistController;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::prefix('reservations')->group(function () {
+        /* Todo: protect the route */
+        Route::get('/', [ReservationController::class, 'index']);
+        /***************************/
+        Route::post('/', [ReservationController::class, 'store']);
+        Route::get('/{code}', [ReservationController::class, 'show'])
+            ->where('code', '^([1-9]\d{0,}_[1-9]\d{0,})$');
+        Route::put('/{code}/cancel', [ReservationController::class, 'cancel'])
+            ->where('code', '^([1-9]\d{0,}_[1-9]\d{0,})$');
+    });
+    Route::prefix('specialists')->group(function () {
+        Route::get('/', [SpecialistController::class, 'index']);
+        Route::get('/{id}', [SpecialistController::class, 'show']);
+    });
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
 });
