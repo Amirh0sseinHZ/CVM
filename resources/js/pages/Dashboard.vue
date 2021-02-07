d531f24acdaa36edb5207e49131159471409088a
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 01:22:21 +0200

    New Dashboard page

---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Welcome back, {{User.name}}</h1>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from "vuex";
+
+    export default {
+    computed: {
+        ...mapGetters({User: 'stateUser'}),
+    }
+}
+</script>
