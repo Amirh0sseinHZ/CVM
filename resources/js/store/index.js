551c4a7616fb6369c10fed2595520eb0f7b62b43
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 01:24:24 +0200

    Vuex instance

---
+++ resources/js/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import auth from './modules/auth'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  modules: {
+    auth: auth
+  },
+  plugins: [createPersistedState()]
+})
