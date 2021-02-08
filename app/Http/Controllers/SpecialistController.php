ccf1d3d0a5e212be5e617396944d1f2a9d94cb41
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 15:29:59 +0200

    clean up

--- app/Http/Controllers/SpecialistController.php
+++ app/Http/Controllers/SpecialistController.php
@@ -3,21 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 
 class SpecialistController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
     public function index()
     {
         $specialists = User::all();
 
+        foreach ($specialists as $specialist) {
+            $specialist->awaiting = $specialist->getWaitingReservations()->count();
+        }
+
         return response()->json([
-            "status"      => 200,
-            "response"    => ['specialists' => $specialists]
+            "status"   => 200,
+            "response" => ['specialists' => $specialists]
         ], 200);
     }
 
@@ -25,7 +30,7 @@ class SpecialistController extends Controller
      * Display the specified resource.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
     public function show($id)
     {
