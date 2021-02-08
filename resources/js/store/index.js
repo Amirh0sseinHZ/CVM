cab5bea0a49bebcfb5c50b961fe209ccb7164e4e
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 15:26:39 +0200

    Register reservation module

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import auth from './modules/auth'
+import reservations from "./modules/reservations";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  strict: process.env.NODE_ENV !== 'production',
-  modules: {
-    auth: auth
-  },
-  plugins: [createPersistedState()]
+    strict: process.env.NODE_ENV !== 'production',
+    modules: {
+        auth: auth,
+        reservations: reservations
+    },
+    plugins: [createPersistedState()]
 })
