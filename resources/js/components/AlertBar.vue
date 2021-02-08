38e730094a7fa45f314cdd8cbee59d016fbede96
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 15:22:51 +0200

    New AlertBar component to handle alerts

---
+++ resources/js/components/AlertBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div :class="style" class="flex-1 p-3 text-lg rounded">
+        <span>{{ text }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AlertBar",
+    props: {
+        text: {
+            type: String,
+            default: "API error, please try again later :("
+        },
+        type: {
+            type: String,
+            default: "danger"
+        }
+    },
+    computed: {
+        style() {
+            switch (this.type) {
+                case 'danger': return "bg-red"
+                case 'warn': return "bg-orange text-black"
+                case 'success': return "bg-green"
+                default: return "bg-red";
+            }
+        }
+    }
+}
+</script>
