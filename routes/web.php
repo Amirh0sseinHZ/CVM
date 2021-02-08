400c8a602833c688b8f095e95c61e0edd20d5c89
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 17:55:13 +0200

    Fix the issue that wouldn't let the deeper level addresses to be routed and handled

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::any('{slug}', function () {
+Route::get('{all}', function() {
     return view('index');
-});
+})->where('all', '.*');
 
