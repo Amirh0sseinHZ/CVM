dbe9f0acac0549de4b72cbb782f1061b0e0afac1
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 15:31:43 +0200

    Experimental rendering the reservation

--- resources/js/components/TheReservationDetails.vue
+++ resources/js/components/TheReservationDetails.vue
@@ -1,5 +1,51 @@
 <template>
     <div>
-        <h1>Your Reservation code is: 1XHR</h1>
+        <h1>Your Reservation code is: {{ reservation.reservation.code }}</h1>
+        <p>{{ timeRemaining }}</p>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        reservation: Object
+    },
+    data() {
+        return {
+            countDown: this.reservation.estimatedWaitingTime,
+            timeRemaining: ''
+        }
+    },
+    created() {
+        this.countDownTimer()
+    },
+    watch: {
+        reservation: function(newVal, oldVal) {
+            this.countDown = newVal.estimatedWaitingTime
+        }
+    },
+    methods: {
+        countDownTimer() {
+            if(this.countDown > 0) {
+                setTimeout(() => {
+                    this.countDown -= 1
+                    this.countDownTimer()
+                    this.timeRemaining = this.secondsToHms(this.countDown)
+                }, 1000)
+            }
+        },
+        secondsToHms(d) {
+            d = Number(d);
+            const h = Math.floor(d / 3600);
+            const m = Math.floor(d % 3600 / 60);
+            const s = Math.floor(d % 3600 % 60);
+
+            const hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
+            const mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
+            const sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
+            return hDisplay + mDisplay + sDisplay;
+        }
+
+    }
+}
+</script>
