9218605e4435977522a83f572543c8ce9daf6f90
k1lleR8 <amirziaee12@gmail.com>
2021-02-08 15:26:09 +0200

    Extracted the active reservation logic to its own component

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,23 +1,33 @@
 <template>
     <div>
-        <the-reservation-details v-if="hasActiveReservation"></the-reservation-details>
+        <the-reservation-details
+            v-if="hasActiveReservation"
+            :reservation="activeReservation"
+        ></the-reservation-details>
+<!--        <div v-if="hasActiveReservation">{{activeReservation}}</div>-->
         <the-book-reservation v-else></the-book-reservation>
     </div>
 </template>
 
 <script>
-import TheBookReservation from '../components/TheBookReservation';
-import TheReservationDetails from '../components/TheReservationDetails';
+    import TheBookReservation from '../components/TheBookReservation';
+    import TheReservationDetails from '../components/TheReservationDetails';
 
-export default {
-    data() {
-        return {
-            hasActiveReservation: false
-        };
+    export default {
+    created() {
+        this.$store.dispatch('checkForActiveReservation')
     },
     components: {
         TheBookReservation,
         TheReservationDetails
-    }
+    },
+    computed: {
+        hasActiveReservation: function() {
+            return this.$store.getters.hasActiveReservation
+        },
+        activeReservation: function() {
+            return this.$store.getters.getTheReservation
+        }
+    },
 }
 </script>
