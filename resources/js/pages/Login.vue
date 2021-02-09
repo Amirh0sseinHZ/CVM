378aa4cdfb404343b2ec7e887c4cb787dcc344d2
k1lleR8 <amirziaee12@gmail.com>
2021-02-09 20:43:12 +0200

    Adjustments

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,27 +1,30 @@
 <template>
-    <form @submit.prevent="login">
-        <BaseInput
-            type="email"
-            label="Email"
-            name="email"
-            v-model="email"
-            autocomplete="email"
-            placeholder="admin@nfq.lt"
-            class="mb-2"
-            required="required"
-        />
-        <BaseInput
-            type="password"
-            label="Password"
-            name="password"
-            v-model="password"
-            class="mb-4"
-            required="required"
-        />
-        <div class="flex justify-between">
-            <BaseBtn type="submit" text="Login"/>
-        </div>
-    </form>
+    <div class="w-full">
+        <h1 class="page-title">Personnel Login</h1>
+        <form @submit.prevent="login">
+            <BaseInput
+                type="email"
+                label="Email"
+                name="email"
+                v-model="email"
+                autocomplete="email"
+                placeholder="admin@nfq.lt"
+                class="mb-2"
+                required="required"
+            />
+            <BaseInput
+                type="password"
+                label="Password"
+                name="password"
+                v-model="password"
+                class="mb-4"
+                required="required"
+            />
+            <div class="flex justify-between">
+                <BaseBtn class="bg-blue-light" type="submit" text="Login"/>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -30,29 +33,29 @@
     import {flash} from "../utils/helpers";
 
     export default {
-    name: "LoginView",
-    components: {
-        BaseBtn,
-        BaseInput
-    },
-    data() {
-        return {
-            email: null,
-            password: null
-        };
-    },
-    methods: {
-        login() {
-            const payload = {
-                email: this.email,
-                password: this.password,
-            };
-            this.$store.dispatch('login', payload).then(() => {
-                this.$router.push({ name: 'Dashboard'});
-            }).catch(err => {
-                flash(err);
-            });
+        name: "LoginView",
+        components: {
+            BaseBtn,
+            BaseInput
         },
-    },
-};
+        data() {
+            return {
+                email: null,
+                password: null
+            };
+        },
+        methods: {
+            login() {
+                const payload = {
+                    email: this.email,
+                    password: this.password,
+                };
+                this.$store.dispatch('login', payload).then(() => {
+                    this.$router.push({name: 'Dashboard'});
+                }).catch(err => {
+                    flash(err);
+                });
+            },
+        },
+    };
 </script>
