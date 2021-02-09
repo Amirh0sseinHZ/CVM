bc0a1fe1d6294d71a8d334502de64625bafea19f
k1lleR8 <amirziaee12@gmail.com>
2021-02-09 20:37:12 +0200

    Improve conditional rendering & Exception handling & Style adjustment

--- resources/js/components/TheNavigation.vue
+++ resources/js/components/TheNavigation.vue
@@ -1,16 +1,21 @@
 <template>
-    <div v-if="!isLoggedIn" class="text-xl select-none outline-none">
-        <router-link class="router-link" :to="{ name: 'Home' }">Home</router-link>
-        <router-link class="router-link" :to="{ name: 'Login' }">Login</router-link>
-    </div>
-    <div v-else class="text-xl select-none outline-none">
-        <router-link class="router-link" :to="{ name: 'Dashboard' }">Dashboard</router-link>
-        <a @click="logout" class="router-link router-link-active">Logout</a>
+    <div class="text-xl select-none outline-none text-gray">
+        <div v-if="!isLoggedIn">
+            <router-link class="router-link" :to="{ name: 'Home' }">Home</router-link>
+            <router-link class="router-link" :to="{ name: 'Login' }">Login</router-link>
+        </div>
+        <div v-else>
+            <router-link class="router-link" :to="{ name: 'SDS' }">SDS</router-link>
+            <router-link class="router-link" :to="{ name: 'Dashboard' }">Dashboard</router-link>
+            <a @click="logout" class="router-link router-link-active">Logout</a>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+    import {notify} from "../utils/helpers";
+
+    export default {
     computed: {
         isLoggedIn: function() {return this.$store.getters.isLoggedIn}
     },
@@ -18,6 +23,8 @@ export default {
         logout() {
             this.$store.dispatch('logout').then(() => {
                 this.$router.push({ name: 'Login'});
+            }).catch(err => {
+                notify('Failed to logout, please try again later.', 'error', 'API Error');
             });
         }
     }
