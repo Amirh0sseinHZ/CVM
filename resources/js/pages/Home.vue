7126c62609c91ffb0744cbacbcf90c545ea6778d
k1lleR8 <amirziaee12@gmail.com>
2021-02-09 20:44:37 +0200

    Checking for active reservation was no longer needed. The logic was removed

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,33 +1,18 @@
 <template>
     <div>
-        <the-reservation-details
-            v-if="hasActiveReservation"
-            :reservation="activeReservation"
-        ></the-reservation-details>
-<!--        <div v-if="hasActiveReservation">{{activeReservation}}</div>-->
-        <the-book-reservation v-else></the-book-reservation>
+        <the-book-reservation></the-book-reservation>
     </div>
 </template>
 
 <script>
     import TheBookReservation from '../components/TheBookReservation';
-    import TheReservationDetails from '../components/TheReservationDetails';
 
     export default {
     created() {
         this.$store.dispatch('checkForActiveReservation')
     },
     components: {
-        TheBookReservation,
-        TheReservationDetails
-    },
-    computed: {
-        hasActiveReservation: function() {
-            return this.$store.getters.hasActiveReservation
-        },
-        activeReservation: function() {
-            return this.$store.getters.getTheReservation
-        }
-    },
+        TheBookReservation
+    }
 }
 </script>
