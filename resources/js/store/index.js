49ae2aed20b2a4931fc852bc2900efeb0cc2bc1e
k1lleR8 <amirziaee12@gmail.com>
2021-02-09 20:43:30 +0200

    Remove unused module

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,15 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import auth from './modules/auth'
-import reservations from "./modules/reservations";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     strict: process.env.NODE_ENV !== 'production',
     modules: {
-        auth: auth,
-        reservations: reservations
+        auth: auth
     },
     plugins: [createPersistedState()]
 })
