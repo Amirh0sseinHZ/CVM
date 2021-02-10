19d0968f1be3b4941cc28f3e3d789774d728dea4
k1lleR8 <amirziaee12@gmail.com>
2021-02-10 17:26:16 +0200

    New Exception introduced to handle 404

---
+++ app/Exceptions/ResourceNotFound404Exception.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+class ResourceNotFound404Exception extends Exception
+{
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @return JsonResponse
+     */
+    public function render()
+    {
+        return response()->json([
+            "message" => "The requested resource was not found."
+        ], 404);
+    }
+}
