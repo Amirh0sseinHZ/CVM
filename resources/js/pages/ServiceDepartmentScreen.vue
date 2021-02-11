2582c59b98bec452e1bb2c04571518f48f6a412c
k1lleR8 <amirziaee12@gmail.com>
2021-02-11 15:42:36 +0200

    Style changes

--- resources/js/pages/ServiceDepartmentScreen.vue
+++ resources/js/pages/ServiceDepartmentScreen.vue
@@ -4,18 +4,19 @@
         <div v-if="reservations.handling || reservations.waiting" class="flex flex-col">
             <div v-if="reservations.handling" class="mb-5">
                 <h2 class="text-xl mb-4">Current Sessions</h2>
-                <div v-for="reservation in reservations.handling" class="bg-white flex justify-between items-center rounded-md text-gray-dark py-3 px-5 mb-3 text-lg font-extrabold">
+                <div v-for="reservation in reservations.handling"
+                     class="bg-white flex justify-between items-center rounded-md text-gray-dark py-3 px-5 mb-3 text-lg font-extrabold">
                     <span>{{ reservation.specialist_id + '-' + reservation.id }}</span>
                     <div class="blink w-4 h-4 bg-green rounded-full"></div>
                 </div>
             </div>
             <div v-if="reservations.waiting">
                 <h2 class="text-xl mb-4">Upcoming Reservations</h2>
-                <div class="flex gap-3 flex-wrap">
-                    <div v-for="reservation in reservations.waiting" class="flex-custom">
-                        <div class="bg-white py-10 text-center rounded-md text-gray-dark text-lg font-extrabold">
-                            <span>{{ reservation.specialist_id + '-' + reservation.id }}</span>
-                        </div>
+                <div class="flex flex-col">
+                    <div
+                        v-for="reservation in reservations.waiting"
+                        class="bg-white flex justify-between items-center rounded-md text-gray-dark py-3 px-5 mb-3 text-lg font-extrabold">
+                        <span>{{ reservation.specialist_id + '-' + reservation.id }}</span>
                     </div>
                 </div>
             </div>
@@ -63,10 +64,6 @@
 </script>
 
 <style scoped>
-    .flex-custom{
-        flex: 1 1 26%;
-    }
-
     .blink {
         animation: blinker 0.7s linear infinite;
     }
