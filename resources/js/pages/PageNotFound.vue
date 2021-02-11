bd1ec1b7236abba1949223cea2500f28033e17ac
k1lleR8 <amirziaee12@gmail.com>
2021-02-11 13:29:16 +0200

    More styling and emoji

--- resources/js/pages/PageNotFound.vue
+++ resources/js/pages/PageNotFound.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div class="w-full text-center text-5xl leading-relaxed">
         <h1>Page Not Found</h1>
+        <span class="text-8xl">☹</span>
     </div>
 </template>
