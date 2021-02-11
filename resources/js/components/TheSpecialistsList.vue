32d2a37798105c3408c8f3c02ea471e4f2f7288c
k1lleR8 <amirziaee12@gmail.com>
2021-02-11 09:40:13 +0200

    New Design

--- resources/js/components/TheSpecialistsList.vue
+++ resources/js/components/TheSpecialistsList.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="flex flex-wrap">
+    <div class="flex flex-col">
         <alert-bar v-if="error"></alert-bar>
         <div
-            v-on:click="select(index)"
-            :class="{selected: specialist.selected}"
             v-for="(specialist, index) in specialists"
-            class="specialist relative select-none text-center outline-none border-solid border-2 border-white rounded-bl-2xl rounded-tr-2xl flex items-center justify-center py-10 my-2 cursor-pointer">
-            <span>{{specialist.name}}</span>
-            <div :style="'background-color:' + statusColor(specialist.awaiting)" class="absolute bottom-2 right-2 w-2 h-2 rounded-full"></div>
+            class="bg-white flex justify-between items-center border-b-4 text-blue-dark rounded-md p-5 mb-5"
+            :class="'border-' + statusColor(specialist.awaiting)"
+            v-on:click="select(index)">
+                <span class="text-lg font-bold mr-3">{{ specialist.name }}</span>
+                <div
+                    class="relative borders border-2 border-gray outline-red rounded-full w-6 h-6"
+                    :class="{'bg-blue-lighter': specialist.selected}">
+                </div>
         </div>
     </div>
 </template>
@@ -26,7 +29,7 @@
     },
     created() {
         axios.get("/api/v1/specialists").then(r => {
-            this.specialists = r.data.response.specialists.map(specialist => {
+            this.specialists = r.data.map(specialist => {
                 return {...specialist, selected: false}
             });
         }).catch(() => this.error = true);
@@ -50,15 +53,16 @@
 </script>
 
 <style scoped>
-.specialist {
-    flex: 1 0 34%;
-    -webkit-tap-highlight-color:  rgba(255, 255, 255, 0);
-}
-.specialist.selected {
-    border-color: #ffe593;
-    pointer-events: none;
-}
-.specialist:nth-child(even) {
-    margin-left: 10px;
+.borders:before {
+    content: " ";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border: 2px solid #ffffff;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
 }
 </style>
